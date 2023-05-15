18f9ae4d796cf11a47df6ecad4e8245c24ac8eed
rdaguas <aguas.rdv2001@gmail.com>
2023-05-14 22:14:03 -0500

    Tarea 3

---
+++ src/main/java/com/example/demo/PaU1P4TRaApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4TRaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4TRaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Repaso Tarea 3");
+	}
+
+}
